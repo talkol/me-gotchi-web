e47ac5be6155fd37e4f5696e8e10208ae9c46e05
Tal Kol <talkol@gmail.com>
2025-07-11 17:50:20 +0000

    Correct models

--- src/ai/services.ts
+++ src/ai/services.ts
@@ -77,7 +77,7 @@ export async function generateAppearanceCharacterAsset(
   const photoDataUri = await fileToDataURI(data.photo);
   
   const response = await openai.responses.create({
-    model: 'gpt-4o',
+    model: 'gpt-4.1-mini',
     input: [
         {
             role: 'user',
@@ -95,8 +95,9 @@ export async function generateAppearanceCharacterAsset(
     ],
     tools: [{
         type: 'image_generation',
+        moderation: 'low',
         quality: 'high',
-        size: '1024x1536',
+        size: '1024x1024',
     }]
   });
 
@@ -141,7 +142,7 @@ export async function generateAppearanceExpressionsAsset(
   const prompt = "Create a square 1:1 image with transparent background and divide it into 9 equal squares. In each square put this face of the boy with a different varied facial expression. Top row: big happy smile mouth closed with eyes open; huge happy smile mouth closed with eyes open; huge laugh mouth open and eyes closed. Middle row: no smile with eyes looking top left; no smile with eyes looking straight; no smile with eyes looking bottom right. Bottom row: big sad frown with eyes open; huge angry frown with eyes open; huge frown crying with eyes closed and tears.";
 
   const response = await openai.responses.create({
-    model: 'gpt-4o',
+    model: 'gpt-4.1-mini',
     input: [
       {
         role: 'user',
@@ -156,6 +157,7 @@ export async function generateAppearanceExpressionsAsset(
         type: 'image_generation',
         size: '1024x1024',
         quality: 'high',
+        moderation: 'low',
         background: 'transparent',
       },
     ],
