e24aa4da671bbda959c2e10f019b8cddd46d1454
Tal Kol <talkol@gmail.com>
2025-07-13 01:55:18 +0000

    ok let's return to shining more light about our issue without trying to

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,9 @@
 
+"use client";
+
 import { initializeApp, getApps, type FirebaseApp } from "firebase/app";
+import { getFunctions } from "firebase/functions";
+import { setLogLevel } from "firebase/app";
 
 // This configuration is for the CLIENT-SIDE app and will be bundled.
 // It uses the NEXT_PUBLIC_ variables from your .env.local file.
@@ -15,11 +19,14 @@ const firebaseConfig = {
 let app: FirebaseApp;
 
 // Initialize Firebase.
-// This will throw an error if the config is invalid.
 if (!getApps().length) {
     app = initializeApp(firebaseConfig);
 } else {
     app = getApps()[0];
 }
+
+// Set log level to 'verbose' to get detailed debugging information
+// in the browser console. This will help us understand the internal error.
+setLogLevel('verbose');
 
 export { app };
