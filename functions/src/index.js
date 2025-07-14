9ad0f996a5a00ec55fe189fa96dced62397977ef
Your Name <talkol@gmail.com>
2025-07-14 14:53:38 +0000

    Forgot to export food icons

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,9 +1,11 @@
 import {initializeApp} from "firebase-admin/app";
 import {generateAssetAppearanceCharacterImp} from "./generateAssetAppearanceCharacter.js";
 import {generateAssetAppearanceExpressionsImp} from "./generateAssetAppearanceExpressions.js";
+import {generateAssetFoodIconsImp} from "./generateAssetFoodIcons.js"; 
 
 // Initialize Firebase Admin SDK
 initializeApp();
 
 export const generateAssetAppearanceCharacter = generateAssetAppearanceCharacterImp;
 export const generateAssetAppearanceExpressions = generateAssetAppearanceExpressionsImp;
+export const generateAssetFoodIcons = generateAssetFoodIconsImp;
