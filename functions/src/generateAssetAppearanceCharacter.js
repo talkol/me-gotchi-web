1f108104c5fc9c099f2b19d8a15de4dc9e55c15e
Your Name <talkol@gmail.com>
2025-07-17 11:10:28 +0000

    Fixed promots for character and expressions

--- functions/src/generateAssetAppearanceCharacter.js
+++ functions/src/generateAssetAppearanceCharacter.js
@@ -58,7 +58,7 @@ export const generateAssetAppearanceCharacterImp = onCall({timeoutSeconds: 300},
           content: [
             {
               type: "input_text",
-              text: `Create a game character based on the likeness of this photo. Focus on the face and make an illustration. White background. The style should not be photo-realistic but should be a detailed illustration.`,
+              text: `Create a game character based on the likeness of this photo. Focus on the face and make it recognizable as much as possible. The art style is stylized 2D illustration with detailed 3D-like shading and detailed facial features. Highly expressive face maintaining the same age as the original photo. The top of the character's head is visible and not cropped, the character is centered and looking forward directly at the camera, hands dropped to the sides. Zoom to focus on the face and chest only. White background.`,
             },
             {
               type: "input_image",
@@ -70,7 +70,7 @@ export const generateAssetAppearanceCharacterImp = onCall({timeoutSeconds: 300},
       tools: [
         {
           type: "image_generation",
-          size: "1024x1536",
+          size: "1024x1024",
           quality: "high",
           moderation: "low",
         },
